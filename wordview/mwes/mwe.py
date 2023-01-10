c77314cf2a226e1ac4b3a679a7ed18d0c0944ac3
meghdadFar <meghdad.farahmand@gmail.com>
2023-01-10 21:27:00 +0100

    Clean up

--- wordview/mwes/mwe.py
+++ wordview/mwes/mwe.py
@@ -1,12 +1,10 @@
-import os
-import sys
 import pandas
 import json
 from pathlib import Path
 from typing import List, Dict
 from nltk import word_tokenize
 from wordview.mwes.am import calculate_am
-from wordview.mwes.mwe_utils import replace_mwes, get_counts
+from wordview.mwes.mwe_utils import get_counts
 from wordview import logger
 
 
@@ -25,25 +23,6 @@ class MWE(object):
             df: DataFrame with a text_column that contains the corpus.
             text_column: Specifies the column of DataFrame where text data resides.
             mwe_types: Types of MWEs to be extracted. Supports: NC for Noun-Noun and JNC for Adjective-Noun compounds. Example: ['NC', 'JNC'].
-            
-            # TODO rm below
-            # output_dir: Output directory where counts and MWEs are stored.
-            # count_dir: Directory where count_file is sotred.
-            # count_file: File in which counts are sotred.
-            # mwe_dir: Directory where mwe_file is sotred.
-            # mwe_file: File in which MWEs are sotred.
-            
-            tokenize: Tokenize the content of `text_column`.
-            
-            #TODO
-            Remove all dir stuff and replace them with the following params:
-            When `build_counts()` receives an input arg filename, it stores the counts, otherwise return counts.
-            
-            When `extract_mwes()` receives an input arg: count filename, it reads the counts there, 
-            otherwise it runs `build_counts()`. When `extract_mwes()` receives a MWE filename argument,
-            it stores MWEs there as json. Otherwise, it returns the corresponding dictionary. 
-
-
 
         Returns:
             None
@@ -54,15 +33,6 @@ class MWE(object):
             if mt not in ["NC", "JNC"]:
                 raise ValueError(f"{mt} type is not recognized.")
         self.mwe_types = mwe_types
-        
-        # self.output_dir = output_dir
-        # self.count_dir = os.path.join(self.output_dir, "counts")
-        # self.count_file = os.path.join(self.count_dir, "count_data.json")
-        # self.mwe_dir = os.path.join(self.output_dir, "mwes")
-        # self.mwe_file = os.path.join(self.mwe_dir, "mwe_data.json")
-
-        # Path(self.output_dir).mkdir(exist_ok=True)
-
         if tokenize:
             logger.info(
                 '"tokenize" flag set to True. This might lead to a slow instantiation.'
@@ -123,21 +93,8 @@ class MWE(object):
         res = get_counts(
             df=self.df, text_column=self.text_column, mwe_types=self.mwe_types
         )
-        # Directory
-        # try:
-        #     Path(self.count_dir).mkdir(exist_ok=True)
-        # except Exception as e:
-        #     logger.error(e)
-        #     raise e
-        # File
         if not counts_filename:
             return res
-            # try:
-            #     with open(self.count_file, "w") as file:
-            #         json.dump(res, file)
-            # except Exception as e:
-            #     logger.error(e)
-            #     raise e
         else:
             try:
                 with open(counts_filename, "w") as file:
@@ -182,21 +139,3 @@ class MWE(object):
         else:
             return mwe_am_dict
 
-
-if __name__ == "__main__":
-    import pandas as pd
-    from wordview.mwes.mwe_utils import replace_mwes
-
-    imdb_train = pd.read_csv(
-        "data/imdb_train_sample.tsv", sep="\t", names=["label", "text"]
-    )
-    mwe = MWE(df=imdb_train, mwe_types=["NC", "JNC"], text_column="text", tokenize=True)
-    mwe.build_counts()
-    mwe.extract_mwes(am="npmi")
-    new_df = replace_mwes(
-        path_to_mwes="tmp/mwes/mwe_data.json",
-        mwe_types=["NC", "JNC"],
-        df=imdb_train,
-        text_column="text",
-    )
-    new_df.to_csv("tmp/new_df.csv", sep="\t")
