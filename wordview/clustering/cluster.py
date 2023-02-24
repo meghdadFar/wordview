6e875e16a764b1d80f11682010afee6af0f90d42
meghdadFar <meghdad.farahmand@gmail.com>
2023-02-24 14:02:26 +0100

    Rm experimental main

--- wordview/clustering/cluster.py
+++ wordview/clustering/cluster.py
@@ -40,35 +40,3 @@ class Cluster:
         for i in range(len(self.documents)):
             clusters[clust_labels[i]].append(self.documents[i])
         self.clusters = clusters
-
-
-if __name__ == "__main__":
-
-    documents = [
-        "Human machine interface for lab abc computer applications",
-        "A survey of user opinion of computer system response time",
-        "The EPS user interface management system",
-        "System and human system engineering testing of EPS",
-        "Relation of user perceived response time to error measurement",
-        "The generation of random binary unordered trees",
-        "The intersection graph of paths in trees",
-        "Graph minors IV Widths of trees and well quasi ordering",
-        "Graph minors A survey",
-    ]
-
-    cl = Cluster(documents=documents, vector_model="tfidf")
-    cl.cluster(clustering_algorithm="AgglomerativeClustering")
-    pprint(cl.clusters)
-
-    # true_k = 2
-    # model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
-    # model.fit(X)
-
-    # print("Top terms per cluster:")
-    # order_centroids = model.cluster_centers_.argsort()[:, ::-1]
-    # terms = vectorizer.get_feature_names()
-    # for i in range(true_k):
-    #     print(f"Cluster {i}")
-    #     for ind in order_centroids[i, :10]:
-    #         print(f"\tCluster {terms[ind]}")
-    #     print
