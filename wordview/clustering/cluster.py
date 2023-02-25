c5169eb5d4645d3b1560e40e37f62cb364c52636
meghdadFar <meghdad.farahmand@gmail.com>
2023-02-25 12:48:13 +0100

    Add docstring

--- wordview/clustering/cluster.py
+++ wordview/clustering/cluster.py
@@ -6,13 +6,21 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 class Cluster:
-    def __init__(self, documents: List[str], vector_model: str) -> None:
+    """Cluster text documents using various clustering algorithms and based on different vectorization technologies."""
+
+    def __init__(self, documents: List[str], vector_model: str = "tfidf") -> None:
+        """
+        Args:
+            documents (List): List of documents.
+            vector_model (str): Vectorization technology. Default = tfidf.
+        """
         self.documents = documents
         self.vector_model = vector_model
         self.vectors = self._vectorize()
         self.clusters: Dict[str, List] = {}
 
     def _vectorize(self):
+        """Vectorizes documents in self.documents using the model specified by self.vector_model."""
         if self.vector_model == "transformer":
             vectorizer = SentenceTransformer("all-MiniLM-L6-v2")
             vectors = vectorizer.encode(self.documents)
@@ -23,7 +31,15 @@ class Cluster:
             raise ValueError(f"Vector model {self.vector_model} is not supported.")
         return vectors
 
-    def cluster(self, clustering_algorithm: str):
+    def cluster(self, clustering_algorithm: str) -> None:
+        """Cluster documents using the algorithm specified by clustering_algorithm.
+
+        Args:
+            clustering_algorithm (str): Clustering algorithm. Supported algorithms: [AgglomerativeClustering].
+
+            Returns:
+                None
+        """
         if clustering_algorithm == "AgglomerativeClustering":
             clust_model = AgglomerativeClustering(
                 n_clusters=None, distance_threshold=1.5
