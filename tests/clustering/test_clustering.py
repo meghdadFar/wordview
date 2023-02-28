b95f83a476d919ba799db24321f0689491bedbab
meghdadFar <meghdad.farahmand@gmail.com>
2023-02-28 16:22:23 +0100

    Update tests

--- tests/clustering/test_clustering.py
+++ tests/clustering/test_clustering.py
@@ -2,16 +2,18 @@ import pytest
 from wordview.clustering import Cluster
 
 
-def test_tfidf_agglomerative_clusters(documents, clusters):
+def test_tfidf_agglomerative_good_cluster_sizes(documents):
     cl = Cluster(documents=documents, vector_model="tfidf")
-    cl.cluster(clustering_algorithm="AgglomerativeClustering")
-    assert cl.clusters == clusters
+    good_cluster_sizes = [1, 5]
+    for n in good_cluster_sizes:
+        cl.cluster(clustering_algorithm="AgglomerativeClustering", n_clusters=n)
+        assert len(cl.clusters) == n
 
 
-def test_transformer_agglomerative_clusters(documents, clusters):
-    cl = Cluster(documents=documents, vector_model="transformer")
-    cl.cluster(clustering_algorithm="AgglomerativeClustering")
-    assert cl.clusters == clusters
+def test_tfidf_agglomerative_zero_cluster_sizes(documents):
+    cl = Cluster(documents=documents, vector_model="tfidf")
+    with pytest.raises(ValueError):
+        cl.cluster(clustering_algorithm="AgglomerativeClustering", n_clusters=0)
 
 
 @pytest.fixture
@@ -29,16 +31,4 @@ def documents():
     return docs
 
 
-@pytest.fixture
-def clusters():
-    clusts = {0: ['The generation of random binary unordered trees',
-                'The intersection graph of paths in trees',
-                'Graph minors IV Widths of trees and well quasi ordering',
-                'Graph minors A survey'],
-            1: ['Human machine interface for lab abc computer applications',
-                'The EPS user interface management system',
-                'System and human system engineering testing of EPS'],
-            2: ['A survey of user opinion of computer system response time',
-                'Relation of user perceived response time to error measurement']
-            }
-    return clusts
+
