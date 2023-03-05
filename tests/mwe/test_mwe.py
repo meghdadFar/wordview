55ec13a6226d53cdcf1d8b69a262f081fe03faf5
Francis Beeson <leobeeson@gmail.com>
2023-03-05 10:48:00 +0000

    Set up testing module for mwe class and add initial test for testing text is being tokenized.

---
+++ tests/mwe/test_mwe.py
@@ -0,0 +1,14 @@
+import pytest
+import pandas as pd
+from wordview.mwes.mwe import MWE
+
+
+@pytest.fixture
+def dummy_text_panda():
+    text = ["new   york  is the capital     of new  york    state", "new york is called the big apple", "i am going to the big apple"]
+    return pd.DataFrame(data = text, columns = ["text"])
+
+
+def test_mwe_tokenizes_text_with_multiple_whitespaces(dummy_text_panda):
+    mwe = MWE(df = dummy_text_panda, text_column = "text", tokenize=True)
+    assert mwe.df["text"][0] == "new york is the capital of new york state"
