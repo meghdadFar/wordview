162466ded57f764a6ffb7a28be40ff1b4ee2249d
Francis Beeson <leobeeson@gmail.com>
2023-03-09 11:35:19 +0000

    Add test for MWE initialisation with tokenisation disabled.

--- tests/mwe/test_mwe.py
+++ tests/mwe/test_mwe.py
@@ -9,6 +9,11 @@ def dummy_text_panda():
     return pd.DataFrame(data = text, columns = ["text"])
 
 
+def test_mwe_does_not_tokenize_text_with_multiple_whitespaces(dummy_text_panda):
+    mwe = MWE(df = dummy_text_panda, text_column = "text", tokenize=False)
+    assert mwe.df["text"][0] == "new   york  is the capital     of new  york    state"
+
+
 def test_mwe_tokenizes_text_with_multiple_whitespaces(dummy_text_panda):
     mwe = MWE(df = dummy_text_panda, text_column = "text", tokenize=True)
     assert mwe.df["text"][0] == "new york is the capital of new york state"
