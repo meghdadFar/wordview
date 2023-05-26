97c1184d6539959fcbf6f5b7ddc3e16867d52718
Francis Beeson <leobeeson@gmail.com>
2023-05-26 21:07:17 +0000

    Fix broken tests because of misnamed fixture.

--- tests/mwe/test_mwe.py
+++ tests/mwe/test_mwe.py
@@ -15,7 +15,7 @@ def dummy_text_pandas():
 
 
 @pytest.fixture
-def dummy_text_panda_with_a_noun_compund():
+def dummy_text_pandas_with_a_noun_compund():
     text = [
         "this sentence has a noun compound",
         ]
@@ -26,7 +26,7 @@ dummy_pos_tags_with_noun_compund = [("this", "XXX"),("sentence", "XXX"),("has", 
 
 
 @pytest.fixture
-def dummy_text_panda_with_no_noun_compund():
+def dummy_text_pandas_with_no_noun_compund():
     text = [
         "no sequence of nouns in this one",
         ]
@@ -38,49 +38,49 @@ dummy_pos_tags_without_noun_compund = [("no", "XXX"),("sequence", "XXX"),("of", 
 
 class TestMweInitialisation:
 
-    def test_mwe_does_not_tokenize_text_with_multiple_whitespaces(self, dummy_text_panda):
-        mwe = MWE(df = dummy_text_panda, text_column = "text", tokenize=False)
+    def test_mwe_does_not_tokenize_text_with_multiple_whitespaces(self, dummy_text_pandas):
+        mwe = MWE(df = dummy_text_pandas, text_column = "text", tokenize=False)
         assert mwe.df["text"][0] == "new   york  is the capital     of new  york    state"
 
 
-    def test_mwe_tokenizes_text_with_multiple_whitespaces(self, dummy_text_panda):
-        mwe = MWE(df = dummy_text_panda, text_column = "text", tokenize=True)
+    def test_mwe_tokenizes_text_with_multiple_whitespaces(self, dummy_text_pandas):
+        mwe = MWE(df = dummy_text_pandas, text_column = "text", tokenize=True)
         assert mwe.df["text"][0] == "new york is the capital of new york state"
 
 
-    def test_mwe_with_wrong_mwe_type_raises_value_error(self, dummy_text_panda):
+    def test_mwe_with_wrong_mwe_type_raises_value_error(self, dummy_text_pandas):
         with pytest.raises(ValueError):
-            mwe = MWE(df = dummy_text_panda, text_column = "text", mwe_types = ["XXX"])
+            mwe = MWE(df = dummy_text_pandas, text_column = "text", mwe_types = ["XXX"])
 
 
-    def test_mwe_with_empty_mwe_type_raises_value_error(self, dummy_text_panda):
+    def test_mwe_with_empty_mwe_type_raises_value_error(self, dummy_text_pandas):
         with pytest.raises(ValueError):
-            mwe = MWE(df = dummy_text_panda, text_column = "text", mwe_types = [])
+            mwe = MWE(df = dummy_text_pandas, text_column = "text", mwe_types = [])
 
 
-    def test_mwe_with_non_list_mwe_type_raises_type_error(self, dummy_text_panda):
+    def test_mwe_with_non_list_mwe_type_raises_type_error(self, dummy_text_pandas):
         with pytest.raises(TypeError):
-            mwe = MWE(df = dummy_text_panda, text_column = "text", mwe_types = "NC")
+            mwe = MWE(df = dummy_text_pandas, text_column = "text", mwe_types = "NC")
 
 
 class TestMweCounter:
 
     @patch("wordview.mwes.mwe.get_pos_tags", MagicMock(return_value = dummy_pos_tags_with_noun_compund))
-    def test_mwe_if_nc_present_returns_counts_with_nc(self, dummy_text_panda_with_a_noun_compund):
-        mwe = MWE(df = dummy_text_panda_with_a_noun_compund, text_column = "text", tokenize=True, mwe_types = ["NC"])
+    def test_mwe_if_nc_present_returns_counts_with_nc(self, dummy_text_pandas_with_a_noun_compund):
+        mwe = MWE(df = dummy_text_pandas_with_a_noun_compund, text_column = "text", tokenize=True, mwe_types = ["NC"])
         counts = mwe.get_counts()
         assert counts["NC"] == {"noun compound": 1}
 
 
     @patch("wordview.mwes.mwe.get_pos_tags", MagicMock(return_value=dummy_pos_tags_without_noun_compund))
-    def test_mwe_if_no_nc_returns_empty_mwe_counts(self, dummy_text_panda_with_no_noun_compund):
-        mwe = MWE(df = dummy_text_panda_with_no_noun_compund, text_column = "text", tokenize=True, mwe_types = ["NC"])
+    def test_mwe_if_no_nc_returns_empty_mwe_counts(self, dummy_text_pandas_with_no_noun_compund):
+        mwe = MWE(df = dummy_text_pandas_with_no_noun_compund, text_column = "text", tokenize=True, mwe_types = ["NC"])
         counts = mwe.get_counts()
         assert counts["NC"] == {}
 
 
 @pytest.mark.xfail
-def test_mwe_build_counts(dummy_text_panda_with_no_noun_compund):
-    mwe = MWE(df = dummy_text_panda_with_no_noun_compund, text_column = "text", tokenize=True, mwe_types = ["NC"])
+def test_mwe_build_counts(dummy_text_pandas_with_no_noun_compund):
+    mwe = MWE(df = dummy_text_pandas_with_no_noun_compund, text_column = "text", tokenize=True, mwe_types = ["NC"])
     counts = mwe.get_counts()
     assert False
