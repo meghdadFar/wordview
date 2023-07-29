4da8607e8b43b0bd046b9c2868d7409b5468b0cb
meghdadFar <meghdad.farahmand@gmail.com>
2023-07-29 19:16:43 +0200

    Refactor and some docstring

--- wordview/preprocessing/count.py
+++ wordview/preprocessing/count.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from collections import defaultdict
 from nltk.util import ngrams
-from nltk.tokenize import sent_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
+import string
+from typing import List, Dict
 
 class DataFrameReader:
     def __init__(self, dataframe, column_name):
@@ -11,9 +13,11 @@ class DataFrameReader:
 
     def get_sentences(self):
         for text in self.data:
+            # Split the text into individual sentences
             sentences = sent_tokenize(text)
             for sentence in sentences:
                 yield sentence
+
 
 class NgramExtractor:
     def __init__(self, dataframe, column_name):
@@ -22,15 +26,24 @@ class NgramExtractor:
 
     def extract_ngrams(self):
         for sentence in self.reader.get_sentences():
-            tokens = sentence.split()
+            # Tokenize and remove punctuations
+            tokens = [word for word in word_tokenize(sentence) if word not in string.punctuation]
+            
             for n in range(1, 5):
                 for ngram in ngrams(tokens, n):
+                    # Convert the n-gram tuple to a space-separated string
                     ngram_str = ' '.join(ngram)
                     self.ngram_counts[ngram_str] += 1
 
-    def get_ngram_counts(self):
+    def get_ngram_counts(self) -> Dict[str, int]:
+        """Returns a dictionary of n-grams and their counts.
+        
+        Args:
+            None
+        Returns:
+            ngram_counts (Dict[str, int]): A dictionary of n-grams and their counts.
+        """
         return dict(self.ngram_counts)
-
 
 if __name__ == "__main__":
     df = pd.DataFrame({
@@ -45,5 +58,6 @@ if __name__ == "__main__":
     extractor.extract_ngrams()
 
     ngram_counts = extractor.get_ngram_counts()
-    for ngram, count in ngram_counts.items():
-        print(f"{ngram}: {count}")
+    print(ngram_counts)
+    # for ngram, count in ngram_counts.items():
+        # print(f"{ngram}: {count}")
