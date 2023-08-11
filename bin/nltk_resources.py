84187e3c5926d69528ddcb11ea77fa62182c3d1c
meghdadFar <meghdad.farahmand@gmail.com>
2023-08-11 14:48:33 +0200

    Improve ccheck nltk resources

--- bin/nltk_resources.py
+++ bin/nltk_resources.py
@@ -1,12 +1,18 @@
 import nltk
+import os
 
 
 def check_nltk_resources():
-    resources = ['punkt',
-                 'stopwords',
-                 'averaged_perceptron_tagger']
-    for res in resources:
-        try:
-            nltk.data.find(f'tokenizers/{res}')
-        except LookupError:
-            nltk.download(res)
+    nltk_data_path = os.path.expanduser('~/nltk_data/')
+    
+    resources = {
+        'tokenizers/punkt': 'punkt',
+        'corpora/stopwords': 'stopwords',
+        'taggers/averaged_perceptron_tagger': 'averaged_perceptron_tagger'
+    }
+
+    for path, package in resources.items():
+        if not os.path.exists(os.path.join(nltk_data_path, path)):
+            nltk.download(package)
+        else:
+            pass
