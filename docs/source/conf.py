c7b752bd28a8b7deeafac75e3a3860b6c53beacd
meghdadFar <meghdad.farahmand@gmail.com>
2023-08-16 20:26:32 +0200

    Update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'wordview'
 copyright = '2023, Meghdad Farahmand'
 author = 'Meghdad Farahmand'
-release = '"0.1.0"'
+release = '"1.1.2"'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
