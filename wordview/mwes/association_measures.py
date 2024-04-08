ebdbfacfb23dc648a68727395eb62951b47271a0
meghdadFar <meghdad.farahmand@gmail.com>
2024-04-08 09:21:26 +0200

    Add frequency threshold to filter out mispelled and extremely rare MWEs

--- wordview/mwes/association_measures.py
+++ wordview/mwes/association_measures.py
@@ -5,29 +5,17 @@ import math
 class PMICalculator:
     """Calculates the Pointwise Mutual Information (PMI) of an n-gram candidate."""
 
-    def __init__(self, ngram_count_source=None, ngram_count_file_path=None):
+    def __init__(self, ngram_count_source: dict):
         """Initializes a new instance of PMICalculator class.
 
         Args:
             ngram_count_source: A dictionary containing ngram counts.
-            ngram_count_file_path: A path to a json file containing ngram counts.
 
         Returns:
             None
         """
-        if ngram_count_source:
-            self.counts = ngram_count_source
-        elif ngram_count_file_path:
-            self.counts = self._load_ngram_counts(ngram_count_file_path)
-        else:
-            raise ValueError("Either count_source or count_file_path must be provided.")
-
+        self.counts = ngram_count_source
         self.total_count = sum(self.counts.values())
-
-    def _load_ngram_counts(self, count_file_path) -> dict[str, int]:
-        with open(count_file_path, "r") as file:
-            counts = json.load(file)
-        return counts
 
     def _prob(self, ngram) -> float:
         return self.counts.get(ngram, 0) / self.total_count
